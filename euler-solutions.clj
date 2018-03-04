c5e7d57499ab9215e45369a4093eb7056cc6a1f4
Dogxx000 <scot0530@morris.umn.edu>
2018-03-03 20:30:07 -0600

    Finish p1

---
+++ euler-solutions.clj
@@ -0,0 +1,15 @@
+; Problem 1
+; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+; Find the sum of all the multiples of 3 or 5 below 1000.
+(defn multiple?
+    "Helper function to (problem-1). Takes a number,
+    and returns it if the number is a multiple of 3 or 5, or returns false if it is not."
+    [n]
+    (if (or (== (mod n 3) 0) (== (mod n 5) 0))
+        n
+        false))
+(defn problem-1 []
+    "Solution to problem-1. Generates a range of integers from 0 to 1000,
+    filters out numbers that are not multiples of 3 or 5, and sums the remaining sequence.
+    The result is 233168."
+    (reduce + (filter multiple? (range 0 1000))))
