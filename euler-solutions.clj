034a7e25024d01fd8d815f9c1d7dbe4446f6dc35
Dogxx000 <scot0530@morris.umn.edu>
2018-03-03 20:32:39 -0600

    Finish p4

--- euler-solutions.clj
+++ euler-solutions.clj
@@ -13,3 +13,16 @@
     filters out numbers that are not multiples of 3 or 5, and sums the remaining sequence.
     The result is 233168."
     (reduce + (filter multiple? (range 0 1000))))
+
+; Problem 4
+;A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+;Find the largest palindrome made from the product of two 3-digit numbers.
+;(require '[clojure.string :as str])
+(defn is-palindrome? [n]
+    (= (str n) (str/reverse (str n))) n)
+(defn problem-4 []
+    "Solution to problem-4. This function needs clojure.string as formatted in the comment above.
+    It finds every combination of three digit integers multiplied together, and filters out the
+    non-palindromic numbers. It then finds the maximum value of the remaining sequence.
+    The result is 996004."
+    (apply max (filter is-palindrome? (for [x (range 100 999) y (range 100 999)] (* x y)))))
