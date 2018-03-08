180773aaa34a087239e2652bf801bdba2963c8ab
Dogxx000 <scot0530@morris.umn.edu>
2018-03-07 20:36:37 -0600

    Add doc string to problem9

--- euler-solutions.clj
+++ euler-solutions.clj
@@ -46,6 +46,11 @@
 (defn pythagorean-triplet? [vec]
     (let [a (vec 0)
           b (vec 1)
+;A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+;a2 + b2 = c2
+;For example, 32 + 42 = 9 + 16 = 25 = 52.
+;There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+;Find the product abc.
           c (- 1000 a b)]
           (and (> c 0) (== (+ (* a a) (* b b)) (* c c)))
     ))
