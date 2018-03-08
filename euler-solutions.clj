d178e8932cc4e3fda6df40cf89d41fa910893b68
Dogxx000 <scot0530@morris.umn.edu>
2018-03-07 20:37:37 -0600

    Refactor var names in helper functions for p9

--- euler-solutions.clj
+++ euler-solutions.clj
@@ -43,22 +43,21 @@
     (apply max (filter is-palindrome? (for [x (range 100 999) y (range 100 999)] (* x y)))))
 
 ; Problem 9
-(defn pythagorean-triplet? [vec]
-    (let [a (vec 0)
-          b (vec 1)
 ;A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 ;a2 + b2 = c2
 ;For example, 32 + 42 = 9 + 16 = 25 = 52.
 ;There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 ;Find the product abc.
+(defn pythagorean-triplet? [v]
+    (let [a (v 0)
+          b (v 1)
           c (- 1000 a b)]
           (and (> c 0) (== (+ (* a a) (* b b)) (* c c)))
     ))
 
-(defn multiply [obj]
-    (let [vec (obj 0)
-          a (vec 0)
-          b (vec 1)
+(defn multiply [s]
+    (let [a (s 0)
+          b (s 1)
           c (- 1000 a b)]
           (* a b c)))
 
